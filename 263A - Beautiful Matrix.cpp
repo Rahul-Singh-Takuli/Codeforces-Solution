7ddcc4cd720629c15fe6709639bfce7b8b44199c
Rahul Singh Takuli <112228408+Rahul-Singh-Takuli@users.noreply.github.com>
2022-10-15 18:57:07 +0530

    Create 263A - Beautiful Matrix.cpp

---
+++ 263A - Beautiful Matrix.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fl for(int i=0;i<n;i++)
+#define fl1 for(int i=1;i<=n;i++)
+#define ll long long
+
+void solve() {
+	int x = 0, n = 5;
+	fl1{
+		for (int j = 1; j <= n; j++) {
+			cin >> x;
+			if (x == 1)
+				cout << abs(i - 3) + abs(j - 3) << endl;
+		}
+	}
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	// ll t; cin >> t;
+	// while (t--) {
+	solve();
+	// }
+}
